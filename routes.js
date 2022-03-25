47d8120895a2568f174458846706cd17589912f9
mvinimariano <64707813+mvinimariano@users.noreply.github.com>
2022-03-25 10:51:19 +0000

    Add files via upload

--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
 const   express = require("express"),
         router = express.Router(),
-        itemCtrl = require("./item-controller");
+        itemCtrl = require("./item-controller"),
+        userCtrl = require("./user-controller");
 
 router.get("/:foo/:bar", itemCtrl.helloWorld);
+router.post('/users', userCtrl.createUser);
+router.get('/users', userCtrl.getUsers);
 
 module.exports = router;
